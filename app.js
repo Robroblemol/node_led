23f7788cc3f256378c77b5bcf438227fb525599e
Raziel <robertodavidrobles@gmail.com>
2017-04-16 14:56:29 -0500

    add index.html a style.css

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ app = express();
 server = require('http').createServer(app);
 io = require('socket.io').listen(server);	//web socket server
 
-server.listen(8080); //start the webserver on port 8080
-app.use(express.static('public')); //tell the server that ./public/ contains the static webpages
-
-
 //inicializamos puerto serialport
 
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
 
+server.listen(8080); //start the webserver on port 8080
+app.use(express.static('public')); //tell the server that ./public/ contains the static webpages
 //definimos websocket
 
 var brightness = 0; //static variable to hold the current brightness
 io.sockets.on('connection', function (socket) { //gets called whenever a client connects
-    socket.emit('led', {value: brightness}); //send the new client the current brightness
-
     socket.on('led', function (data) { //makes the socket react to 'led' packets by calling this function
         brightness = data.value;  //updates brightness from the data object
         var buf = new Buffer(1); //creates a new 1-byte buffer
@@ -26,4 +22,6 @@ io.sockets.on('connection', function (socket) { //gets called whenever a client 
 
         io.sockets.emit('led', {value: brightness}); //sends the updated brightness to all connected clients
     });
+    socket.emit('led', {value: brightness}); //send the new client the current brightness
 });
+console.log("runnig");
